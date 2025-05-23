bb9ab4d793b09166f51fc0ffe43945bb7b1a6fc7
Chinmaya Kolhe <chinmayakolhe2005@gmail.com>
2025-05-23 16:45:07 +0530

    Added first program files

--- src/Inputs.java
+++ src/Inputs.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+public class Inputs{
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the int");
+        int rollno = input.nextInt();
+        System.out.println(rollno);
 
-public class Inputs {
-}
+        System.out.println("Enter the float");
+        float salary=input.nextFloat();
+        System.out.println(salary);
+
+        System.out.println("Enter the String");
+        String s1=input.next();
+        System.out.println(s1);
+
+        System.out.println("Enter the Double");
+        double wages=input.nextDouble();
+        System.out.println(wages);
+
+    }
+} 
