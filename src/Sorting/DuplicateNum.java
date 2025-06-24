be2adc3d9d49de8a4f1f0f96bbba26e2c57ac4ec
Chinmaya Kolhe <chinmayakolhe2005@gmail.com>
2025-06-24 16:46:28 +0530

    Sorting ex

---
+++ src/Sorting/DuplicateNum.java
@@ -0,0 +1,38 @@
+package Sorting;
+
+public class DuplicateNum {
+    public static void main(String[] args) {
+        int[]arr={1,3,2,2,4};
+        System.out.println(duplicatenum(arr));
+    }
+    static int duplicatenum(int[] arr){
+        int i=0;
+
+        while(i<arr.length){
+            int current=arr[i]-1;
+            if(arr[i]<arr.length && arr[i]!=arr[current]){
+
+                swap(arr,i,current);
+                
+            }
+            else{
+                i++;
+            }
+        }
+
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                return arr[index];
+            }
+        }
+
+        return -1;
+
+    }
+
+    private static void swap(int[] arr, int i, int i1) {
+        int temp=arr[i];
+        arr[i]=arr[i1];
+        arr[i1]=temp;
+    }
+}
