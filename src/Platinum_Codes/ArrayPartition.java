5453bf70e577eda210e4fffbce0db13cc3e9c65a
Chinmaya Kolhe <chinmayakolhe2005@gmail.com>
2025-07-25 15:10:13 +0530

    Platinum Batch codes

---
+++ src/Platinum_Codes/ArrayPartition.java
@@ -0,0 +1,18 @@
+package Platinum_Codes;
+import java.util.Arrays;
+public class ArrayPartition {
+    static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum=0;
+        for(int i=0;i<nums.length;i+=2){
+            sum+=nums[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[]nums={1,4,3,2};
+        System.out.println(arrayPairSum(nums));
+    }
+
+}
